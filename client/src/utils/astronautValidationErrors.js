6feb39adee466ead82a42d229e43faf56776303f
meq <fewell@gmail.com>
2019-03-26 15:41:16 -0400

    the complete errors object is now returned

--- client/src/utils/astronautValidationErrors.js
+++ client/src/utils/astronautValidationErrors.js
@@ -1,11 +1,11 @@
 import { attributeValidators } from './attributeValidators';
 
 export default function astronautValidationErrors(astronaut) {
+  let collectedErrors
   Object.keys(attributeValidators).reduce((errors, validator) => {
     errors[validator] = !attributeValidators[validator](astronaut)
+    collectedErrors = errors
+    return errors
   }, {})
-  return errors;
+  return collectedErrors;
 }
-
-
-
